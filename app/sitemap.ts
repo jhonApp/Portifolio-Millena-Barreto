8b775e95fa6b23938c5440de215b79a6fea48289
Jhon Everton <jhonevertonapp@outlook.com>
2025-03-23 14:31:57 -0300

    Merge: JH012 >>> Master: (SEO) - include new sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,36 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next'
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url:'http://millenabarreto.com.br/'
-    }
+      url: 'https://millenabarreto.com.br/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://millenabarreto.com.br/#redes-sociais',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://millenabarreto.com.br/#trabalhos',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://millenabarreto.com.br/#clientes',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://millenabarreto.com.br/#contate-me',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
   ]
 }
