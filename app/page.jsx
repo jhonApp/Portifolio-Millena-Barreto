bf899bcc0878e6c9317132cb8b7bd55c05143e89
Jhon Everton <jhonevertonapp@outlook.com>
2025-10-05 10:07:24 -0300

    Impletad improves such as sitemap and index google

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-"use client";
 import { useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://millenabarreto.com.br',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
